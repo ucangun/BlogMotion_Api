807c9370a69cd7bf38ce8610e468071de4ba2ffb
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-07 23:18:20 +0100

    "Initialize BlogMotion API with Express and MongoDB connection"

--- index.js
+++ index.js
@@ -3,3 +3,53 @@
 /* ------------------------------------------------- */
 /*                  BLOGMOTION API                   */
 /* ------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+/* ------------------------------------------------- */
+
+// envVariables to process.env:
+require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
+const PORT = process.env?.PORT || 8000;
+
+/* ------------------------------------------------- */
+// Configrations:
+
+// DB Connection
+const connectDB = require("./src/configs/dbConnection");
+connectDB();
+
+/* ------------------------------------------------- */
+
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json());
+
+/* ------------------------------------------------- */
+
+// Routes:
+
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to BlogMotion Api",
+    // documents: {
+    //   swagger: "/documents/swagger",
+    //   redoc: "/documents/redoc",
+    //   json: "/documents/json",
+    // },
+  });
+});
+
+/* ------------------------------------------------- */
+
+// RUN SERVER:
+app.listen(PORT, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
+
+/* ------------------------------------------------------- */
