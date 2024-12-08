3000504c3ca97edbe4397da448dedf7fad6f0418
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-08 15:32:46 +0100

    `Added bcryptjs and validator dependencies, updated user model to use these dependencies, and added password hashing and validation functionality.`

--- src/models/user.js
+++ src/models/user.js
@@ -5,38 +5,87 @@
 /* ------------------------------------------------- */
 
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const validatePassword = require("../helpers/validatePassword");
 
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, "Username is required"],
       unique: true,
     },
     password: {
       type: String,
       required: true,
+      validate: {
+        validator: validatePassword,
+        message:
+          "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+      },
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
     },
+
     firstName: {
       type: String,
-      required: true,
+      required: [true, "First name is required"],
     },
+
     lastName: {
       type: String,
-      required: true,
+      required: [true, "Last name is required"],
     },
+
     image: {
       type: String,
       default: "default-avatar.jpg",
     },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   { collection: "users", timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 module.exports = mongoose.model("User", userSchema);
